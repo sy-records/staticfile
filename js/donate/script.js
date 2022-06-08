71c2e39b9c2cdafb17127d9f7070ecb02ff0e2a9
沈唁 <52o@qq52o.cn>
2022-06-08 15:14:58 +0800

    Update script.js

--- js/donate/script.js
+++ js/donate/script.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function () {
     var QRBox = $('#QRBox');
     var MainBox = $('#MainBox');
-    var AliPayQR = 'https://cdn.jsdelivr.net/gh/sy-records/staticfile/images/donate/tip_qrcode_alipay.jpg';
-    var WeChanQR = 'https://cdn.jsdelivr.net/gh/sy-records/staticfile/images/donate/tip_qrcode_wechatpay.jpg';
+    var AliPayQR = 'https://fastly.jsdelivr.net/gh/sy-records/staticfile/images/donate/tip_qrcode_alipay.jpg';
+    var WeChanQR = 'https://fastly.jsdelivr.net/gh/sy-records/staticfile/images/donate/tip_qrcode_wechatpay.jpg';
 
     function showQR(QR) {
         if (QR) {
